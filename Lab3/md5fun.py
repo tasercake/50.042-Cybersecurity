8b01e7ce7a9cb154458b8cd5b4f110dd44e4b0e5
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-10-10 23:02:04 +0800

    [WIP] Lab3

--- Lab3/md5fun.py
+++ Lab3/md5fun.py
@@ -1,67 +1,86 @@
 #!/usr/bin/env python3
+"""
+Krishna Penukonda
+1001781
+
+Timings:
+- hash5.txt: Generation 70.3s; Lookup 4.6s (Found 15/15)
+- salt6.txt: Generation 68.5; Lookup 3.76s (Found 9/15)
+"""
 import string
 import hashlib
 import argparse
 from time import time
-from random import shuffle
+from pathlib import Path
+from random import shuffle, choice
 from functools import lru_cache
 from itertools import product
-from multiprocessing import Pool
 
 
 def md5hash(msg):
-    return msg, hashlib.md5(msg).hexdigest()
+    return hashlib.md5(msg).hexdigest()
 
 
 def get_rainbow_table(candidates):
-    table = {}
-    # table = {md5hash(c): c for c in candidates}
-    pool = Pool()
-    for c, h in pool.imap_unordered(md5hash, candidates, chunksize=1024):
-        table[h] = c
+    table = {md5hash(c): c for c in candidates}
     return table
 
 
-def get_candidates():
-    characters = list((string.ascii_lowercase + string.digits))
+def get_candidates(charset):
+    characters = list(charset)
     shuffle(characters)
     characters = ''.join(characters).encode('utf8')
     candidates = map(bytes, product(characters, repeat=5))
     return candidates
 
 
-def crack(md5):
-    for c in get_candidates():
-        h = hashlib.md5(c).hexdigest()
-        if h == md5:
-            return c
-    print(f"No matching password for {md5}")
-
-
 def main(args):
-    with open(args.file, 'r') as file:
-        hashes = [h.strip() for h in file.readlines()]
-    start = time()    
-    candidates = get_candidates()
-    table = get_rainbow_table(candidates)
-    print(f"Rainbow: {time() - start}s")
+    hash_file = Path('hash5.txt')
+    pass_file = Path('pass5.txt')
+    charset = string.ascii_lowercase + string.digits
+    with open(hash_file, 'r') as f:
+        hashes = [h.strip() for h in f.readlines()]
 
-    start = time()
-    messages = [table[h] for h in hashes]    
-    print(messages)
-    print(f"Lookup time: {time() - start}s")
+    if pass_file.exists():
+        print("Passwords already cracked! Loading from file...")
+        with open(pass_file, 'r') as f:
+            passwords = [l.strip() for l in f.readlines()]
+    else:
+        start = time()
+        # Candidate password generator
+        candidates = get_candidates(charset)
+        # Compute rainbow table
+        table = get_rainbow_table(candidates)
+        print(f"Rainbow time: {time() - start}s")
+        # Lookup passwords in generated rainbow table
+        passwords = [table[h].decode('utf-8') for h in hashes]
 
-    # pool = Pool()
-    # messages = pool.imap(crack, hashes)
-    # for i, m in enumerate(messages):
-    #     elapsed = time() - start
-    #     print(f"{i + 1}: Found hash({m}) at t={elapsed:.3f}s")
-    # print(f"Cracked {len(hashes)} hashes in {elapsed:.3f}s")
-    # print(dict(zip(messages, hashes)))
+        # Save passwords to file
+        with open(pass_file, 'w') as f:
+            for h, p in zip(hashes, passwords):
+                f.write(f"{p}\n")
+    for h, p in zip(hashes, passwords):
+        print(f"{h} -> {p}")
+
+    if args.salt:
+        # Append random character to each password (salt)
+        salted = [p + choice(charset) for p in passwords]
+        # Save salted passwords
+        salted_pass_file = Path('pass6.txt')
+        with open(salted_pass_file, 'w') as f:
+            for p in salted:
+                f.write(f"{p}\n")
+        # Hash salted passwords
+        salted_hashed = [md5hash(p.encode('utf-8')) for p in salted]
+        # Save hashsed salted passwords
+        salted_hash_file = Path('salt6.txt')
+        with open(salted_hash_file, 'w') as f:
+            for p in salted_hashed:
+                f.write(f"{p}\n")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('file')
+    parser.add_argument('--salt', action='store_true')
     args = parser.parse_args()
     main(args)
