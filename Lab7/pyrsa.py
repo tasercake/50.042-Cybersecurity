5b07f24cc373db00608ba9572a03f60752093b85
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-11-21 01:16:36 +0800

    RSA Encryption, Decryption, Signature Verification

--- Lab7/pyrsa.py
+++ Lab7/pyrsa.py
@@ -1,26 +1,68 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
-from base64 import b64decode,b64encode
+from base64 import b64decode, b64encode
 from Crypto.Signature import PKCS1_PSS
 from Crypto.Hash import SHA256
 import argparse
 import sys
+from pyrsa_sq_mul import square_multiply, pack_bigint, unpack_bigint
+
 
 def generate_RSA(bits=1024):
-    pass
-    return private_key, public_key
-    
-def encrypt_RSA(public_key_file,message):
-    pass
+    rsa = RSA.generate(bits)
+    return rsa, rsa.publickey()
 
-def decrypt_RSA(private_key_file,cipher):
-    pass
 
-def sign_RSA(private_key_loc,data):
-    pass
+def encrypt_RSA(public_key_file, message):
+    with open(public_key_file, 'r') as key:
+        rsa = RSA.importKey(key.read())
+    unpacked = unpack_bigint(message)
+    cipher = square_multiply(unpacked, rsa.e, rsa.n)
+    packed = bytes(pack_bigint(cipher))
+    return packed
 
-def verify_sign(private_key_loc,sign,data):
-    pass
 
-if __name__=="__main__":
-    pass
+def decrypt_RSA(private_key_file, cipher):
+    with open(private_key_file, 'r') as key:
+        rsa = RSA.importKey(key.read())
+    unpacked = unpack_bigint(cipher)
+    message = square_multiply(unpacked, rsa.d, rsa.n)
+    packed = bytes(pack_bigint(message))
+    return packed
+
+
+def sign_RSA(private_key_file, message):
+    with open(private_key_file, 'r') as key:
+        rsa = RSA.importKey(key.read())
+    hashed = SHA256.new(data=message).digest()
+    unpacked = unpack_bigint(hashed)
+    signature = square_multiply(unpacked, rsa.d, rsa.n)
+    packed = bytes(pack_bigint(signature))
+    return packed
+
+
+def verify_sign(public_key_file, sign, message):
+    with open(public_key_file, 'r') as key:
+        rsa = RSA.importKey(key.read())
+    hashed = SHA256.new(data=message).digest()
+    unpacked = unpack_bigint(sign)
+    return hashed == bytes(pack_bigint(square_multiply(unpacked, rsa.e, rsa.n)))
+
+
+if __name__ == "__main__":
+    with open("message.txt", 'r') as message:
+        message = message.read().encode('utf8')
+
+        print("========== Encryption and Decryption ==========")
+        print(f"Original:\n{message}\n")
+        encrypted = encrypt_RSA("mykey.pem.pub", message)
+        print(f"Encrypted:\n{encrypted}\n")
+        decrypted = decrypt_RSA("mykey.pem.priv", encrypted)
+        print(f"Decrypted:\n{decrypted}\n")
+
+
+        print("========== Signature Verification ==========")
+        signature = sign_RSA("mykey.pem.priv", message)
+        print(f"Signature:\n{signature}\n")
+        verification = verify_sign("mykey.pem.pub", signature, message)
+        print(f"Signature verfied:\n{verification}\n")
--- Lab7/pyrsa_sq_mul.py
+++ Lab7/pyrsa_sq_mul.py
@@ -1,22 +1,31 @@
 from Crypto.PublicKey import RSA
-from base64 import b64encode,b64decode
+from base64 import b64encode, b64decode
 import random
 
-def square_multiply(a,x,n):
-    pass
+
+def square_multiply(a, x, n):
+    y = 1
+    for i in range(x.bit_length(), -1, -1):
+        y = (y ** 2) % n
+        if (x >> i) & 1:
+            y = (a * y) % n
+    return y
+
 
 # function to convert long int to byte string
 def pack_bigint(i):
-    b=bytearray()
+    b = bytearray()
     while i:
-        b.append(i&0xFF)
-        i>>=8
+        b.append(i & 0xFF)
+        i >>= 8
     return b
+
 
 # function to convert byte string to long int
 def unpack_bigint(b):
-    b=bytearray(b)
-    return sum((1<<(bi*8))* bb for (bi,bb) in enumerate(b))
+    b = bytearray(b)
+    return sum((1 << (bi * 8)) * bb for (bi, bb) in enumerate(b))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     pass
