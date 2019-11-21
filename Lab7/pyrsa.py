af3b072f4d0da79e2f41d806dc51f31995e15ab9
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-11-22 01:55:55 +0800

    RSA Encryption Attack

--- Lab7/pyrsa.py
+++ Lab7/pyrsa.py
@@ -5,70 +5,98 @@ Year 2019
 
 Krishna Penukonda - 1001781
 """
+from random import randint
 from Crypto.PublicKey import RSA
-from Crypto.Hash import SHA256
+from Crypto.Cipher import PKCS1_OAEP
 from base64 import b64decode, b64encode
-from pyrsa_sq_mul import square_multiply, pack_bigint, unpack_bigint
+from Crypto.Signature import PKCS1_PSS
+from Crypto.Hash import SHA256
+from pyrsa_sq_mul import pack_bigint, unpack_bigint
 
 
 def generate_RSA(bits=1024):
+    print("Generating a new RSA keypair...")
     rsa = RSA.generate(bits)
+    print("Done!")
     return rsa, rsa.publickey()
 
 
 def encrypt_RSA(public_key_file, message):
     with open(public_key_file, "r") as key:
         rsa = RSA.importKey(key.read())
-    unpacked = unpack_bigint(message)
-    cipher = square_multiply(unpacked, rsa.e, rsa.n)
-    packed = bytes(pack_bigint(cipher))
-    # packed = b64encode(packed)
-    return packed
+    cipher = b64encode(PKCS1_OAEP.new(rsa).encrypt(message))
+    return cipher
 
 
 def decrypt_RSA(private_key_file, cipher):
     with open(private_key_file, "r") as key:
         rsa = RSA.importKey(key.read())
-    # cipher = b64decode(cipher)
-    unpacked = unpack_bigint(cipher)
-    message = square_multiply(unpacked, rsa.d, rsa.n)
-    packed = bytes(pack_bigint(message))
-    return packed
+    message = PKCS1_OAEP.new(rsa).decrypt(b64decode(cipher))
+    return message
 
 
 def sign_RSA(private_key_file, message):
     with open(private_key_file, "r") as key:
         rsa = RSA.importKey(key.read())
-    hashed = SHA256.new(data=message).digest()
-    unpacked = unpack_bigint(hashed)
-    signature = square_multiply(unpacked, rsa.d, rsa.n)
-    packed = bytes(pack_bigint(signature))
-    return packed
+    hashed = SHA256.new(data=message)
+    signature = b64encode(PKCS1_PSS.new(rsa).sign(hashed))
+    return signature
 
 
 def verify_sign(public_key_file, sign, message):
     with open(public_key_file, "r") as key:
         rsa = RSA.importKey(key.read())
-    hashed = SHA256.new(data=message).digest()
-    unpacked = unpack_bigint(sign)
-    return hashed == bytes(pack_bigint(square_multiply(unpacked, rsa.e, rsa.n)))
+    hashed = SHA256.new(data=message)
+    return PKCS1_PSS.new(rsa).verify(hashed, b64decode(sign))
+
+
+def save_key(key, filename, **kwargs):
+    print(f"Saving key '{key}' to '{filename}'...")
+    with open(filename, 'wb') as f:
+        f.write(key.exportKey(**kwargs))
+    print("Saved!")
+
+
+def multiply_encrypted(public_key_file, c1, c2):
+    with open(public_key_file, "r") as key:
+        rsa = RSA.importKey(key.read())
+    return b64encode(pack_bigint(unpack_bigint(b64decode(c1)) * unpack_bigint(b64decode(c2)) % rsa.n))
 
 
 if __name__ == "__main__":
-    print("==============================\nRSA PROTOCOL DEMO - NO PADDING\n==============================")
-    with open("message.txt", "r") as message:
+    public_key_file = "newkey.pem.pub"
+    private_key_file = "newkey.pem.priv"
+
+    print("========================\nPADDED RSA PROTOCOL DEMO\n========================")
+    private_key, public_key = generate_RSA()
+    save_key(public_key, public_key_file)
+    save_key(private_key, private_key_file)
+    print("\n=========== Padded Encryption & Decryption ===========")
+    with open("mydata.txt", "r") as message:
         message = message.read().encode('utf8')
+        print(f"Original message:\n{message}\n")
+    encrypted = encrypt_RSA(public_key_file, message)
+    print(f"Encrypted:\n{encrypted}\n")
+    decrypted = decrypt_RSA(private_key_file, encrypted)
+    print(f"Decrypted:\n{decrypted}\n")
+    print("\n=========== Padded Signature Verification ===========")
+    print(f"Original message:\n{message}\n")
+    signature = sign_RSA(private_key_file, message)
+    print(f"Signature:\n{signature}\n")
+    verification = verify_sign(public_key_file, signature, message)
+    print(f"Signature verfied:\n{verification}\n")
 
-        print("---------- Encryption & Decryption ----------")
-        print(f"Original:\n{message}\n")
-        encrypted = encrypt_RSA("mykey.pem.pub", message)
-        print(f"Encrypted:\n{encrypted}\n")
-        decrypted = decrypt_RSA("mykey.pem.priv", encrypted)
-        print(f"Decrypted:\n{decrypted}\n")
-
-        print("---------- Signature Verification ----------")
-        print(f"Original:\n{message}\n")
-        signature = sign_RSA("mykey.pem.priv", message)
-        print(f"Signature:\n{signature}\n")
-        verification = verify_sign("mykey.pem.pub", signature, message)
-        print(f"Signature verfied:\n{verification}\n")
+    print("============================\nPADDED RSA ENCRYPTION ATTACK\n============================")
+    print("=========== Padded RSA Encryption Attack ===========")
+    message = 100
+    multiplier = 2
+    print(f"Chosen Integer:\n{message}\n")
+    print(f"Multiplier:\n{multiplier}\n")
+    encrypted = encrypt_RSA(public_key_file, pack_bigint(message))
+    print(f"Encrypted Integer:\n{encrypted}\n")
+    encrypted_multiplier = encrypt_RSA(public_key_file, pack_bigint(multiplier))
+    print(f"Encrypted Multiplier:\n{encrypted_multiplier}\n")
+    result = multiply_encrypted(public_key_file, encrypted, encrypted_multiplier)
+    print(f"Multiplication Result:\n{result}\n")
+    decrypted = decrypt_RSA(private_key_file, result)
+    print(f"Decrypted:\n{unpack_bigint(decrypted)}\n")
--- Lab7/pyrsa_sq_mul.py
+++ Lab7/pyrsa_sq_mul.py
@@ -7,7 +7,9 @@ Krishna Penukonda - 1001781
 """
 from Crypto.PublicKey import RSA
 from base64 import b64encode, b64decode
-import random
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from base64 import b64decode, b64encode
 
 
 def square_multiply(a, x, n):
@@ -34,5 +36,88 @@ def unpack_bigint(b):
     return sum((1 << (bi * 8)) * bb for (bi, bb) in enumerate(b))
 
 
+def generate_RSA(bits=1024):
+    rsa = RSA.generate(bits)
+    return rsa, rsa.publickey()
+
+
+def encrypt_RSA(public_key_file, message):
+    with open(public_key_file, "r") as key:
+        rsa = RSA.importKey(key.read())
+    unpacked = unpack_bigint(message)
+    cipher = square_multiply(unpacked, rsa.e, rsa.n)
+    packed = bytes(pack_bigint(cipher))
+    packed = b64encode(packed)
+    return packed
+
+
+def decrypt_RSA(private_key_file, cipher):
+    with open(private_key_file, "r") as key:
+        rsa = RSA.importKey(key.read())
+    cipher = b64decode(cipher)
+    unpacked = unpack_bigint(cipher)
+    message = square_multiply(unpacked, rsa.d, rsa.n)
+    packed = bytes(pack_bigint(message))
+    return packed
+
+ 
+def sign_RSA(private_key_file, message):
+    with open(private_key_file, "r") as key:
+        rsa = RSA.importKey(key.read())
+    hashed = SHA256.new(data=message).digest()
+    unpacked = unpack_bigint(hashed)
+    signature = square_multiply(unpacked, rsa.d, rsa.n)
+    packed = bytes(pack_bigint(signature))
+    packed = b64encode(packed)
+    return packed
+
+
+def verify_sign(public_key_file, sign, message):
+    with open(public_key_file, "r") as key:
+        rsa = RSA.importKey(key.read())
+    hashed = SHA256.new(data=message).digest()
+    unpacked = unpack_bigint(b64decode(sign))
+    return hashed == bytes(pack_bigint(square_multiply(unpacked, rsa.e, rsa.n)))
+
+
+def multiply_encrypted(public_key_file, c1, c2):
+    with open(public_key_file, "r") as key:
+        rsa = RSA.importKey(key.read())
+    return b64encode(pack_bigint(unpack_bigint(b64decode(c1)) * unpack_bigint(b64decode(c2)) % rsa.n))
+
+
 if __name__ == "__main__":
-    pass
+    public_key_file = "mykey.pem.pub"
+    private_key_file = "mykey.pem.priv"
+
+    print("==============================\nRSA PROTOCOL DEMO - NO PADDING\n==============================")
+    with open("message.txt", "r") as message:
+        message = message.read().encode('utf8')
+    print("========== Encryption & Decryption ==========")
+    print(f"Original:\n{message}\n")
+    encrypted = encrypt_RSA(public_key_file, message)
+    print(f"Encrypted:\n{encrypted}\n")
+    decrypted = decrypt_RSA(private_key_file, encrypted)
+    print(f"Decrypted:\n{decrypted}\n")
+    print("========== Signature Verification ==========")
+    print(f"Original:\n{message}\n")
+    signature = sign_RSA(private_key_file, message)
+    print(f"Signature:\n{signature}\n")
+    verification = verify_sign(public_key_file, signature, message)
+    print(f"Signature verfied:\n{verification}\n")
+
+    print("==================================\nRSA ENCRYPTION ATTACK - NO PADDING\n==================================")
+    print("=========== RSA Encryption Attack ===========")
+    message = 100
+    multiplier = 2
+    print(f"Chosen Integer:\n{message}\n")
+    print(f"Multiplier:\n{multiplier}\n")
+    encrypted = encrypt_RSA(public_key_file, pack_bigint(message))
+    print(f"Encrypted Integer:\n{encrypted}\n")
+    encrypted_multiplier = encrypt_RSA(public_key_file, pack_bigint(multiplier))
+    print(f"Encrypted Multiplier:\n{encrypted_multiplier}\n")
+    result = multiply_encrypted(public_key_file, encrypted, encrypted_multiplier)
+    print(f"Multiplication Result:\n{result}\n")
+    decrypted = decrypt_RSA(private_key_file, result)
+    print(f"Decrypted:\n{unpack_bigint(decrypted)}\n")
+
